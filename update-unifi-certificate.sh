3e418e10c4b358b2d5905d212702415ed847b161
Martin Kamp Jensen <martin.kamp.jensen@gmail.com>
2016-07-30 14:30:00 +0200

    Add basic script for updating the UniFi Controller certificate based on a certificate generated by acme.sh

---
+++ update-unifi-certificate.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# USER CONFIGURATION BEGIN
+
+# The domain for which acme.sh generated/generates a certificate
+DOMAIN="my.domain.tld"
+
+# The path to the acme.sh installation
+ACME_SH="/path/to/.acme.sh"
+
+# USER CONFIGURATION END
+
+echo "Updating UniFi Controller certificate"
+
+WORKDIR="${ACME_SH}/${DOMAIN}"
+
+echo "* Creating PKCS12 keystore..."
+openssl pkcs12 -export -passout pass:aircontrolenterprise \
+ -in ${WORKDIR}/${DOMAIN}.cer \
+ -inkey ${WORKDIR}/${DOMAIN}.key \
+ -out ${WORKDIR}/keystore.pkcs12 -name unifi \
+ -CAfile ${WORKDIR}/fullchain.cer -caname root
+
+echo "* Stopping UniFi controller..."
+systemctl stop unifi
+
+echo "* Importing certificate into Unifi Controller keystore..."
+keytool -noprompt -trustcacerts -importkeystore -deststorepass aircontrolenterprise \
+ -destkeypass aircontrolenterprise -destkeystore /usr/lib/unifi/data/keystore \
+ -srckeystore ${WORKDIR}/keystore.pkcs12 -srcstoretype PKCS12 -srcstorepass aircontrolenterprise -alias unifi
+
+cat > ${WORKDIR}/identrust.cer << EOF
+-----BEGIN CERTIFICATE-----
+MIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/
+MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT
+DkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow
+PzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD
+Ew5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB
+AN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O
+rz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq
+OLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b
+xiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw
+7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD
+aeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV
+HQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG
+SIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69
+ikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr
+AvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz
+R8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5
+JDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo
+Ob8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ
+-----END CERTIFICATE-----
+EOF
+
+echo "* Importing certificate via ace.jar..."
+java -jar /usr/lib/unifi/lib/ace.jar import_cert \
+ ${WORKDIR}/${DOMAIN}.cer \
+ ${WORKDIR}/ca.cer \
+ ${WORKDIR}/identrust.cer
+
+echo "* Starting UniFi Controller..."
+systemctl start unifi
